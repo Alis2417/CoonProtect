07d8aa38866f968757ce79749c3bb1cfbb01d6e5
Alis2417 <allaisaulova17@gmail.com>
2024-06-23 22:40:54 +0300

    feat: added new block whith button

--- frontend/coonproject/src/pages/main/index.js
+++ frontend/coonproject/src/pages/main/index.js
@@ -1,7 +1,8 @@
 import './style.css';
+import Picture from '../../imgs/Picture.png'
 
 function MainPage() {
-  return (
+  return (<>
     <div className="main-page">
       <div class="main-page_content">
       <div class="main-page_container">
@@ -10,7 +11,26 @@ function MainPage() {
       </div>
       </div>
     </div>
+    <div className="features-section">
+      <div class="text-content">
+        <h2>Повысь свой уровень знаний в ИБ</h2>
+        <ul>
+          <li>выбирай один из множества бесплатных курсов</li>
+          <li>получай актуальные прикладные знания</li>
+          <li>повышай свой уровень и применяй на практике</li>
+        </ul>
+        <div class="buttons">
+          <button class="primary-btn">Пройти курс</button>
+          <button class="secondary-btn">Зарегистрироваться</button>
+        </div>
+      </div>
+      <div class="image-content">
+      <img src={Picture} alt="Cyber Security Course"/>
+      </div>
+    </div>
+    </>
   );
 }
+
 
 export default MainPage;
