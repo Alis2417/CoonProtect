4ab23b0eb6599f9d28e29b17cd9e3bd53f9af615
Alis2417 <allaisaulova17@gmail.com>
2024-06-23 19:40:56 +0300

    feat added block 1 with a picture
    added block 1 with a picture and replaced it in the tag <link> & <title> elements

--- frontend/coonproject/src/pages/main/index.js
+++ frontend/coonproject/src/pages/main/index.js
@@ -1,8 +1,14 @@
+import './style.css';
 
 function MainPage() {
   return (
-    <div className="App">
-      main Page
+    <div className="main-page">
+      <div class="main-page_content">
+      <div class="main-page_container">
+      <h1>Безопасность данных в твоих руках</h1> 
+      <p>Пройди бесплатный курс по информационной безопасности и защити свои данные от мошенников</p>
+      </div>
+      </div>
     </div>
   );
 }
