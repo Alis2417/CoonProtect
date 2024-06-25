ac455506b558ef3ace136e9c5e191302a5bc9735
av <aominedaiki3ww@gmail.com>
2024-06-25 10:58:23 +0300

    Mock для данных

---
+++ frontend/coonproject/src/pages/courses/mock.js
@@ -0,0 +1,38 @@
+export const courses = [{
+	id: 1,
+	progress: 95,
+	lessons: 12,
+	akHours: 10,
+	type: 'Онлайн-обучение',
+	title: 'Социальная инженерия',
+	visibility: 'Для всех пользователей',
+	description: 'Узнай о том, какими способами могут обмануть мошенники и как этому противостоять, а также прокачай свои навыки в финансовой грамотности',
+	disabled: false,
+	modules: [
+		{
+			id: 'module_1',
+			name: 'Модуль 1',
+			progress: 100,
+		},
+		{
+			id: 'module_2',
+			name: 'Модуль 2',
+			progress: 100,
+		},
+		{
+			id: 'module_3',
+			name: 'Тестирование',
+			progress: 56,
+		},
+		{
+			id: 'module_4',
+			name: 'Модуль 3',
+			progress: 100,
+		},
+		{
+			id: 'module_5',
+			name: 'Итоговое тестирование',
+			progress: 0,
+		},
+	]
+}]
