a366c85806a04e97c85efed404be6ee122a46d2e
av <aominedaiki3ww@gmail.com>
2024-06-25 11:01:44 +0300

    Добавлены иконки для курсов.

---
+++ frontend/coonproject/src/pages/courses/Icons.js
@@ -0,0 +1,17 @@
+export const Flask = () => (
+	<svg width="61" height="60" viewBox="0 0 61 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path
+			d="M25.4987 5V23.8175C25.4994 24.595 25.3188 25.362 24.9712 26.0575L12.2987 51.375C12.106 51.7575 12.0147 52.183 12.0337 52.6109C12.0527 53.0388 12.1813 53.4546 12.4072 53.8185C12.633 54.1824 12.9486 54.4821 13.3236 54.689C13.6986 54.8959 14.1204 55.003 14.5487 55H46.4487C46.877 55.003 47.2989 54.8959 47.6739 54.689C48.0489 54.4821 48.3644 54.1824 48.5903 53.8185C48.8161 53.4546 48.9447 53.0388 48.9637 52.6109C48.9827 52.183 48.8915 51.7575 48.6987 51.375L36.0262 26.0575C35.6786 25.362 35.498 24.595 35.4987 23.8175V5"
+			stroke="#5E5D5D" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+		<path d="M21.75 5H39.25" stroke="#5E5D5D" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+		<path d="M18 40H43" stroke="#5E5D5D" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+	</svg>
+);
+
+export const IconAdd = () => (
+	<svg width="151" height="150" viewBox="0 0 151 150" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M31.75 75H119.25M75.5 31.25V118.75" stroke="#5E5D5D" strokeWidth="4" strokeLinecap="round"
+		      strokeLinejoin="round"
+		/>
+	</svg>
+)
