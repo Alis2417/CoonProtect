9dfbff8ec8f248a245e31586c09b75c863db30f3
av <aominedaiki3ww@gmail.com>
2024-06-25 11:02:02 +0300

    Сверстана страница курсов.

--- frontend/coonproject/src/pages/courses/index.js
+++ frontend/coonproject/src/pages/courses/index.js
@@ -1,17 +1,42 @@
 import CourseCard from './components/course-card';
+import { CourseProgressCard } from './components/CourseProgressCard';
+import './courses.css';
+import { courses } from './mock';
 
 function CoursesPage() {
     return (
-        <div className="courses-page">
-            <CourseCard
-                title="Пример курса"
-                text="Узнай о том, какими способами могут обмануть мошенники и как этому противостоять, а также прокачай свои навыки в финансовой грамотности"
-                percent={50}
-                inProccess={true}
-                disable={false}
-            />
-            
-        </div>
+      <div className="courses-page">
+          {courses.map(course => (
+            <div className="courses-page-card__row" key={course.id}>
+                <CourseCard
+                  title={course.title}
+                  description={course.description}
+                  progress={course.progress}
+                  inProccess={course.progress < 100}
+                  disabled={course.disabled}
+                  type={course.type}
+                  onClick={() => console.log('click')}
+                />
+                <CourseProgressCard
+                  title='Основные показатели'
+                  akHours={course.akHours}
+                  lessons={course.lessons}
+                  modules={course.modules}
+                />
+            </div>
+          ))}
+          <div className="courses-page-card__row">
+              <CourseCard
+                onAddCourse={() => console.log('add course')}
+                isTemplate
+                templateText='Добавить курс'
+              />
+              <CourseProgressCard
+                isTemplate
+                templateText='У вас больше нет курсов, если хотите пройти еще один, выберете из каталога'
+              />
+          </div>
+      </div>
     );
 }
 
