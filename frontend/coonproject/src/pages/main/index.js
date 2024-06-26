a2291072b605f524013fd6a7632a0748822a8db0
Alis2417 <allaisaulova17@gmail.com>
2024-06-26 23:16:52 +0300

    feat: add block advantage
    Добавила блок с преимуществами проекта

--- frontend/coonproject/src/pages/main/index.js
+++ frontend/coonproject/src/pages/main/index.js
@@ -1,5 +1,8 @@
 import './style.css';
 import Picture from '../../imgs/Picture.png'
+import Thumbs from '../../imgs/thumbs-up.png'
+import Heart from '../../imgs/heart.png'
+import List from '../../imgs/list-checks.png'
 
 function MainPage() {
   return (<>
@@ -28,19 +31,25 @@ function MainPage() {
       <img src={Picture} alt="Cyber Security Course"/>
       </div>
     </div>
-    <div class="courses-container">
-      <div class="course-card large">
-        <img src="../../imgs/Rectangle.png" alt="Курсы ИБ"/>
-        <div class="overlay">Курсы ИБ</div>
+    <div class="project-benefits">
+      <h1>О проекте и  его преимуществах</h1>
+      <p>Проект создан в целях повышения знаний в области информационной безопасности и направлен на минимизацию кибератак на пользователей сети Интернет</p>
+      <div class="benefits">
+      <div class="benefit">
+        <img src={Thumbs} alt="Thumbs-up" />
+        <h2>Актуальные знания в сфере ИБ в одном месте</h2>
+        <p>В проекте представлены только свежие новости в сфере ИБ с постоянным обновлением базы знаний и курсов под новые реалии представляемой сферы</p>
       </div>
-      <div class="course-card small">
-        <div class="overlay">Сoциальная инженерия</div>
+      <div class="benefit">
+        <img src={Heart} alt="Heart"/>
+        <h2>Комфортное обучение без ограничений</h2>
+        <p>Бесплатные курсы позволят получить необходимые знания в области ИБ, а понятный интерфейс сделает обучение приятным</p>
       </div>
-      <div class="course-card small">
-        <div class="overlay">IoT Security</div>
+      <div class="benefit">
+        <img src={List} alt="List"/>
+        <h2>Понятный план обучения</h2>
+        <p>Полученные знания будет легко применять на практике — ты сможешь грамотно  противостоять кибератакам мошенников</p>
       </div>
-      <div class="course-card large blue">
-        <div class="overlay">Подобрать курс</div>
       </div>
     </div>
 
