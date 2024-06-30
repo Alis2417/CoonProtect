b801287f51e86b35490e3aa724ce59426e70b0c8
Alis2417 <allaisaulova17@gmail.com>
2024-06-30 20:57:55 +0300

    feat: add profile card
    Добавила карточку профиля на странице личного кабинета

--- frontend/coonproject/src/pages/profile/components/profile-card.js
+++ frontend/coonproject/src/pages/profile/components/profile-card.js
@@ -1,12 +1,63 @@
 import './profile-card.css'
+import Profile from "../../../imgs/profile.png";
+
 function ProfileCard() {
     return (
       
             <div className="profile-card">
                 <div className="profile-card__short-info">
-                  <img className="profile-card__logo"></img>
+                  <img src={Profile} alt="Profile" className="profile-card__logo"></img>
+                  <div class="profile-info">
+                <p>Кристина</p>
+                <p>ID: 1797364821</p>
+            </div>
                 </div>
                 <div className="profile-card__form">
+
+                  <div className="form-group">
+
+                    <div class="form-container">
+
+                    <div class="form-item">
+                  <label>Фамилия</label>
+                  <input name='surname-person'></input>
+                  </div>
+                  <div class="form-item">
+                  <label>Имя</label>
+                  <input name='name-person'></input>
+                  </div>
+                  <div class="form-item">
+                  <label>Отчество</label>
+                  <input name='patronymic'></input>
+                  </div>
+
+                    </div>
+
+                  <div class="form-container">
+
+                  <div class="form-item">
+                  <label>Email</label>
+                  <input name='email'></input>
+                  </div>
+                  <div class="form-item">
+                  <label>Номер телефона</label>
+                  <input name='phone'></input>
+                  </div>
+                  <div class="form-item">
+                  <label>Пароль</label>
+                  <input name='password'></input>
+                  <a href="#" class="change-password">Изменить пароль</a>
+                  </div>
+
+                  <div className="buttons" type="submit">
+          <button className="secondary">Сохранить</button>
+        </div>
+
+                  </div>
+
+
+                  </div>
+                  
 
                 </div>
             </div>
--- frontend/coonproject/src/pages/profile/index.js
+++ frontend/coonproject/src/pages/profile/index.js
@@ -5,7 +5,7 @@ function ProfilePage() {
     return (
         <div className="App">
            <ProfileCard></ProfileCard>
-           <AppFooter></AppFooter>
+           
         </div>
     );
 }
